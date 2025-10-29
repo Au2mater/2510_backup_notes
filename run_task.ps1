47f133eab00c74e2052597c6802e8ca9ae292f4b
Ahmad K. B. <ab.dev@tutamail.com>
2025-10-29 19:41:50 +0100

    Make relative path absolute for logfile  in run_task.ps1

--- run_task.ps1
+++ run_task.ps1
@@ -2,4 +2,4 @@ Start-ScheduledTask -TaskName 'GitBackupNotesTask'
 Get-ScheduledTaskInfo -TaskName 'GitBackupNotesTask'   # shows LastRunTime & LastTaskResult
 # wait two seconds to allow the task to start and write to the log
 Start-Sleep -Seconds 2
-Get-Content .\backup_notes.log -Tail 100
+Get-Content C:\Users\Bruger\OneDrive\03_Resources\2510_backup_notes\backup_notes.log -Tail 100
